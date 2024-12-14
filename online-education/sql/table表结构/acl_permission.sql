df32f2902647a77ab57a67f84b36275ee0cccb27
邱海梦旌 <88176739+QiuHaimengjing@users.noreply.github.com>
2024-12-14 12:38:16 +0800

    Update acl_permission.sql

--- online-education/sql/table表结构/acl_permission.sql
+++ online-education/sql/table表结构/acl_permission.sql
@@ -8,13 +8,13 @@ create table acl_permission
     type             tinyint          default 0   not null comment '类型(1:菜单,2:按钮)',
     permission_value varchar(50)                  null comment '权限值',
     path             varchar(100)                 null comment '访问路径',
+    redirect         varchar(100)                 null comment '重定向',
     component        varchar(100)                 null comment '组件路径',
     icon             varchar(50)                  null comment '图标',
     status           tinyint                      null comment '状态(0:禁止,1:正常)',
     is_deleted       tinyint unsigned default '0' not null comment '逻辑删除 1（true）已删除， 0（false）未删除',
     gmt_create       datetime                     null comment '创建时间',
-    gmt_modified     datetime                     null comment '更新时间',
-    redirect         varchar(100)                 null comment '重定向'
+    gmt_modified     datetime                     null comment '更新时间'
 )
     comment '权限';
 
